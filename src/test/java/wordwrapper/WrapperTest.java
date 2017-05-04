26de1c97a08be056f0656b17caa64366cf36d9cd
백재현 <jojonari@jojonaliui-MacBook-Pro.local>
2017-05-04 23:37:05 +0900

    Getting Unstuck 실습완료

--- src/test/java/wordwrapper/WrapperTest.java
+++ src/test/java/wordwrapper/WrapperTest.java
@@ -10,13 +10,34 @@ public class WrapperTest {
     @Test
     public void
     should_wrap() {
-        assertThat(wrap("word word", 4), is("word\nword"));
-        assertThat(wrap("a dog", 5), is("a dog"));
-        assertThat(wrap("a dog with a bone", 6), is("a dog\nwith a \nbone"));
+        assertWraps(null, 1, "");
+        assertWraps("", 1, "");
+        assertWraps("x", 1, "x");
+        assertWraps("xx", 1, "x\nx");
+        assertWraps("xxx", 1, "x\nx\nx");
+        assertWraps("x x", 1, "x\nx");
+        assertWraps("x xx", 3, "x\nxx");
+        assertWraps("four score and seven years ago our fathers brought forth upon this continent", 7,
+                "four\nscore\nand\nseven\nyears\nago our\nfathers\nbrought\nforth\nupon\nthis\ncontine\nnt");
 
     }
 
+    private void assertWraps(String s, int width, String expected) {
+        assertThat(wrap(s, width), is(expected));
+    }
+
     private String wrap(String s, int width) {
-        return s.length() > width ? s.replaceAll(" ", "\n") : s;
+        if(s == null)
+            return "";
+        if (s.length() <= width)
+            return s;
+        else{
+            int brackPoint = s.lastIndexOf(" ", width);
+            if (brackPoint == -1) {
+                brackPoint = width;
+            }
+            return s.substring(0, brackPoint) +"\n"+ wrap(s.substring(brackPoint).trim(), width);
+        }
+
     }
 }

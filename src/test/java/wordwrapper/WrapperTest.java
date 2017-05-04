061c50dbaf962db54f0e15020bdf7c6ee64c12f3
백재현 <jojonari@jojonaliui-MacBook-Pro.local>
2017-05-04 23:20:57 +0900

    getting stuck 발생

---
+++ src/test/java/wordwrapper/WrapperTest.java
@@ -0,0 +1,22 @@
+package wordwrapper;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class WrapperTest {
+
+    @Test
+    public void
+    should_wrap() {
+        assertThat(wrap("word word", 4), is("word\nword"));
+        assertThat(wrap("a dog", 5), is("a dog"));
+        assertThat(wrap("a dog with a bone", 6), is("a dog\nwith a \nbone"));
+
+    }
+
+    private String wrap(String s, int width) {
+        return s.length() > width ? s.replaceAll(" ", "\n") : s;
+    }
+}
